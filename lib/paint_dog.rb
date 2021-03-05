f390a00901fd0ec02f13be039c03ffba62269fa6
MMcClure11 <mcclme02@luther.edu>
2021-03-05 10:33:28 -0600

    WIP how to test for a random word? test will pass when word sample matches the test

--- lib/paint_dog.rb
+++ lib/paint_dog.rb
@@ -1,7 +1,17 @@
 class PaintDog
 
   def initialize
-    @word = "joyous"
+    @word = words.sample
+  end
+
+  def words 
+    [
+      ["cricket", "A game played by gentlemen"],
+      ["jogging", "We are not walking..."],
+      ["celebrate", "Remembering special moments"],
+      ["continent", "There are 7 of these"],
+      ["exotic", "Not from around here..."],
+    ]
   end
 
 end
--- spec/01_paint_dog_spec.rb
+++ spec/01_paint_dog_spec.rb
@@ -5,14 +5,7 @@ describe './lib/paint_dog.rb' do
     describe '#initialize' do 
       it 'assigns an instance variable @word to a random word from the words array' do 
         game = PaintDog.new
-        words = [
-          ["cricket", "A game played by gentlemen"],
-          ["jogging", "We are not walking..."],
-          ["celebrate", "Remembering special moments"],
-          ["continent", "There are 7 of these"],
-          ["exotic", "Not from around here..."],
-        ]
-        word = words.sample
+        word = ["exotic", "Not from around here..."]
         expect(game.instance_variable_get(:@word)).to eql(word)
       end
     end
