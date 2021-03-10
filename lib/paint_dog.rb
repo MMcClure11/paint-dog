d1483e7d7cdf674a30f636261d78ef458dd576d8
MMcClure11 <mcclme02@luther.edu>
2021-03-09 19:31:51 -0600

    Add print_teaser method and @word_teaser instance variable

--- lib/paint_dog.rb
+++ lib/paint_dog.rb
@@ -3,6 +3,10 @@ class PaintDog
   def initialize
     @word = words.sample
     @lives = 7
+    @word_teaser = ""
+    @word.first.size.times do 
+      @word_teaser += "_ "
+    end
   end
 
   def words 
@@ -15,12 +19,15 @@ class PaintDog
     ]
   end
 
+  def print_teaser
+    puts @word_teaser
+  end
 
   def play
     #ask user for a letter
     puts "New game started... your word is #{ @word.first.size } characters long"
     puts "To exit the game at any point type 'exit'"
-
+    print_teaser
   end
 
 end
